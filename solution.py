59440f3079bb85abb56a796413dba2a321d2bb05
josecolella <josecolella@yahoo.com>
2017-01-21 11:38:22 +0100

    Changed return to iterator. Added more test cases

--- solution.py
+++ solution.py
@@ -1,40 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
 """
-Copyright 2017 josecolella@yahoo.com
+Copyright (c) 2017 Jose Colella
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
 """
 
 import itertools
 import typing
 
-def fizz_buzz(beginning_number: int=1, ending_number:int=100) ->  list:
-    """Returns a list between `beginning_number` and `ending_number` (inclusive)
+
+def fizz_buzz(beginning_number: int = 1, ending_number: int = 100, key: int=None):
+    """Returns a generator that yields an integer or a string after verifying
+    if the integer from `beginning_number` to `ending_number` is divisible by 3, 5
+    or 15
 
     Args:
         beginning_number: The integer that denotes the beginning of the sequence
         ending_number: The integer that denotes the end of the sequence
+        key: An optional integer that when specified only returns the result for the key
     Returns:
-        A list that contains tuples(int, string) where the string is `ThreeFive`
-        if the integer is divisible by 3 and 5, `Three` if integer is only divisible by 3,
-        `Five` if integer is only divisible by 5.
+        A generator that yields an integer or a string based on the following conditions:
+        - the string is `ThreeFive` if the integer is divisible by 3 and 5,
+        -`Three` if integer is only divisible by 3,
+        - `Five` if integer is only divisible by 5.
+        - the integer if none of the above conditions apply
     """
 
-    if beginning_number > ending_number: raise ValueError("`beginning_number` should be less than `ending_number`")
-    if beginning_number < 0: raise ValueError("`beginning_number` should be greater than 0")
-    if ending_number < 0: raise ValueError("`ending_number` should be greater than 0")
-    # Create set with range from `beginning_number` to `ending_number` inclusive
+    if beginning_number > ending_number:
+        raise ValueError(
+            "`beginning_number` should be less than `ending_number`")
+    if beginning_number < 0:
+        raise ValueError("`beginning_number` should be greater than 0")
+    if ending_number < 0:
+        raise ValueError("`ending_number` should be greater than 0")
+    result_list = None
+    # Create set with range from `beginning_number` to `ending_number`
+    # inclusive
     all_numbers = set(range(beginning_number, ending_number + 1))
-    # Create three buckets; 1. Multiple of 3 and 5. 2. Multiple of 3. 3. Multiple of 5
-    multiple_both = set(filter(lambda x: x % 3 == 0 and x % 5 == 0, all_numbers))
-    multiple_three = set(filter(lambda x: x % 3 == 0 and x not in multiple_both, all_numbers))
-    multiple_five = set(filter(lambda x:x % 5 == 0 and x not in multiple_both and x not in multiple_three, all_numbers))
+    # Create three buckets; 1. Multiple of 3 and 5. 2. Multiple of 3. 3.
+    # Multiple of 5
+    multiple_both = set(filter(lambda x: x % 3 == 0 and x %
+                               5 == 0, all_numbers))
+    multiple_three = set(filter(lambda x: x %
+                                3 == 0 and x not in multiple_both, all_numbers))
+    multiple_five = set(filter(
+        lambda x: x % 5 == 0 and x not in multiple_both and x not in multiple_three, all_numbers))
 
-    # Create a set with the remaining numbers that do not fullfill the conditions
-    remaining_numbers = all_numbers.difference(itertools.chain(multiple_both, multiple_three, multiple_five))
+    # Create a set with the remaining numbers that do not fullfill the
+    # conditions
+    remaining_numbers = all_numbers.difference(
+        itertools.chain(multiple_both, multiple_three, multiple_five)
+    )
 
     # Create transformations for printing
     transform_remaining_numbers = map(lambda x: (x, ''), remaining_numbers)
@@ -42,6 +75,17 @@ def fizz_buzz(beginning_number: int=1, ending_number:int=100) ->  list:
     transform_multiple_five = map(lambda x: (x, 'Five'), multiple_five)
     transform_multiple_three = map(lambda x: (x, 'Three'), multiple_three)
 
-    # Create sorted list, based on the first parameter of the inner tuples
-    result_list = sorted(itertools.chain(transform_remaining_numbers,transform_multiple_both,transform_multiple_five,transform_multiple_three), key=lambda x: x[0])
+    # Create sorted list, that is sorted based on the first index: integer
+    transformed_result_list = sorted(itertools.chain(transform_remaining_numbers,
+                                                     transform_multiple_both,
+                                                     transform_multiple_five,
+                                                     transform_multiple_three
+                                                     ), key=lambda x: x[0])
+    # Create clean result list
+    clean_result_tuple = lambda result_tuple: result_tuple[0] if result_tuple[1] is '' else result_tuple[1]
+    result_list = (clean_result_tuple(trans_tuple)
+                   for trans_tuple in transformed_result_list)
+    if key:
+        result_list = itertools.islice(result_list, key - 1, key)
+
     return result_list
--- tests.py
+++ tests.py
@@ -1,19 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+Copyright (c) 2017 Jose Colella
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
 import unittest
+import types
 import solution
+
 
 class TestFizzBuzz(unittest.TestCase):
 
-    
+    def setUp(self):
+        self.default_beginning_index = 1
+        self.default_ending_index = 100
+
     def test_wrong_type_parameters(self):
         with self.assertRaises(Exception):
-            solution.fizz_buzz('2','100')
+            solution.fizz_buzz('2', '100')
 
     def test_return_type(self):
-        self.assertEqual(list, type(solution.fizz_buzz(1,100)))
+        self.assertEqual(types.GeneratorType, type(solution.fizz_buzz(1, 100)))
 
     def test_length_return(self):
         with self.assertRaises(Exception):
-            solution.fizz_buzz(100,2)
+            solution.fizz_buzz(100, 2)
 
     def test_negative_numbers(self):
         with self.assertRaises(Exception):
@@ -21,23 +49,73 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_output_3_returns_three(self):
         index = 3
-        results = solution.fizz_buzz(1,100)
-        self.assertEqual(results[index - 1][1], 'Three')
+        results = solution.fizz_buzz(self.default_beginning_index, self.default_ending_index, index)
+        self.assertEqual(next(results), 'Three')
 
     def test_output_95_returns_five(self):
         index = 95
-        results = solution.fizz_buzz(1,100)
-        self.assertEqual(results[index - 1][1], 'Five')
+        results = solution.fizz_buzz(self.default_beginning_index, self.default_ending_index, index)
+        self.assertEqual(next(results), 'Five')
 
     def test_output_90_returns_threefive(self):
         index = 90
-        results = solution.fizz_buzz(1,100)
-        self.assertEqual(results[index -1][1], 'ThreeFive')
+        results = solution.fizz_buzz(self.default_beginning_index, self.default_ending_index, index)
+        self.assertEqual(next(results), 'ThreeFive')
 
-    def test_output_16_returns_empty_string(self):
+    def test_output_16_returns_number(self):
         index = 16
-        results = solution.fizz_buzz(1,100)
-        self.assertEqual(results[index -1][1], '')
-        
+        expected_result = 16
+        results = solution.fizz_buzz(self.default_beginning_index, self.default_ending_index, index)
+        self.assertEqual(next(results), expected_result)
+
+    def test_output_30_returns_threefive_string(self):
+        index = 30
+        expected_result = 'ThreeFive'
+        results = solution.fizz_buzz(self.default_beginning_index, self.default_ending_index, index)
+        self.assertEqual(next(results), expected_result)
+
+    def test_output_fizz_buzz_from_one_to_hundred(self):
+        expected_results = [1, 2, 'Three', 4, 'Five', 'Three', 7, 8, 'Three', 'Five', 11,
+                            'Three', 13, 14, 'ThreeFive', 16, 17, 'Three', 19,
+                            'Five', 'Three', 22, 23, 'Three', 'Five', 26, 'Three', 28, 29,
+                            'ThreeFive', 31, 32, 'Three', 34, 'Five', 'Three', 37, 38, 'Three', 'Five',
+                            41, 'Three', 43, 44, 'ThreeFive', 46, 47, 'Three', 49, 'Five',
+                            'Three', 52, 53, 'Three', 'Five', 56, 'Three', 58, 59,
+                            'ThreeFive', 61, 62, 'Three', 64, 'Five', 'Three', 67, 68, 'Three',
+                            'Five', 71, 'Three', 73, 74, 'ThreeFive', 76, 77, 'Three',
+                            79, 'Five', 'Three', 82, 83, 'Three', 'Five', 86, 'Three', 88, 89,
+                            'ThreeFive', 91, 92, 'Three', 94, 'Five', 'Three', 97, 98, 'Three', 'Five']
+        results = list(solution.fizz_buzz(
+            self.default_beginning_index, self.default_ending_index))
+        for expected_result, result in zip(expected_results, results):
+            self.assertEqual(expected_result, result)
+
+    def test_output_empty_parameters_returns_type(self):
+        results = solution.fizz_buzz()
+        self.assertEqual(types.GeneratorType, type(results))
+
+    def test_output_length_fizz_buzz_from_one_to_hundred(self):
+        expected_result = 100
+        results = solution.fizz_buzz(self.default_beginning_index, self.default_ending_index)
+        self.assertEqual(len(list(results)), expected_result)
+
+    def test_output_135_returns_threefive_string(self):
+        expected_result = 'ThreeFive'
+        index = 135
+        results = solution.fizz_buzz(self.default_beginning_index, 200, index)
+        self.assertEqual(next(results), expected_result)
+
+    def test_output_1440_returns_threefive_string(self):
+        expected_result = 'ThreeFive'
+        index = 1440
+        results = solution.fizz_buzz(self.default_beginning_index, 2000, 1440)
+        self.assertEqual(next(results), expected_result)
+
+    def test_output_7368_returns_three_string(self):
+        expected_result = 'Three'
+        index = 7368
+        results = solution.fizz_buzz(self.default_beginning_index, 8000, index)
+        self.assertEqual(next(results), expected_result)
+
 if __name__ == '__main__':
     unittest.main()
